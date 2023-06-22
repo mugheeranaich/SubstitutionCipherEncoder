cb622b3c38929dbd6030171e1a6d81b383c62929
Mugheera <mugheeram567@gmail.com>
2023-06-21 20:36:38 -0400

    Updated three-character substitution encryption algorithm with lowercase letter support

--- encryption.py
+++ encryption.py
@@ -1,13 +1,17 @@
 print("Three character substitution!!\n")
 
-initial_alph = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"," "]
+initial_alph = [ "A","a", "B","b", "C","c", "D","d", "E","e", "F","f", "G","g", "H","h", "I","i", "J","j", "K","k", "L","l", "M","m", "N","n" ,"O","o", "P", "p", "Q","q", "R","r", "S","s", "T","t", "U","u", "V","v", "W","w", "X","x", "Y","y", "Z","z"," "]
 
-encryption_alph = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","A", "B", "C"," "]
+encryption_alph = ["D","d", "E","e", "F","f", "G","g", "H","h", "I","i", "J","j", "K","k", "L","l", "M","m", "N","n" ,"O","o", "P", "p", "Q","q", "R","r", "S","s", "T","t", "U","u", "V","v", "W","w", "X","x", "Y","y", "Z","z","A","a", "B","b", "C","c"," "]
 
-name = input("Enter your message all letters should be uppercase : ")
+name = input("Enter your message: ")
+# print(initial_alph.index[])
+# print(index.[encryption_alph])
+
 data = []
 for i in name:
     c = initial_alph.index(i)
+
     b = encryption_alph[c]
     data.append(b)
 print("".join(data))
