6483b2617ff32769e42d05c87c76607d7decb75b
Mugheera <mugheeram567@gmail.com>
2023-06-21 21:56:40 -0400

    Refactor code for character substitution cipher with updated character substitution lists and enhanced user instructions, input validation, and center alignment for string output.

--- encryption.py
+++ encryption.py
@@ -1,17 +1,32 @@
-print("Three character substitution!!\n")
+import sys
+string ="Three character substitution!!"
+print (string.center(50))
 
-initial_alph = [ "A","a", "B","b", "C","c", "D","d", "E","e", "F","f", "G","g", "H","h", "I","i", "J","j", "K","k", "L","l", "M","m", "N","n" ,"O","o", "P", "p", "Q","q", "R","r", "S","s", "T","t", "U","u", "V","v", "W","w", "X","x", "Y","y", "Z","z"," "]
+print("1:Encyption\n2:Decryption")
+choice = int(input("Enter your choice[1or2]: "))
 
-encryption_alph = ["D","d", "E","e", "F","f", "G","g", "H","h", "I","i", "J","j", "K","k", "L","l", "M","m", "N","n" ,"O","o", "P", "p", "Q","q", "R","r", "S","s", "T","t", "U","u", "V","v", "W","w", "X","x", "Y","y", "Z","z","A","a", "B","b", "C","c"," "]
 
-name = input("Enter your message: ")
-# print(initial_alph.index[])
-# print(index.[encryption_alph])
+initial_alph = [ "A","a", "B","b", "C","c", "D","d", "E","e", "F","f", "G","g", "H","h", "I","i", "J","j", "K","k", "L","l", "M","m", "N","n" ,"O","o", "P", "p", "Q","q", "R","r", "S","s", "T","t", "U","u", "V","v", "W","w", "X","x", "Y","y", "Z","z"," ","0","1","2","3","4","5","6","7","8","9"]
 
-data = []
-for i in name:
-    c = initial_alph.index(i)
+encryption_alph = ["D","d", "E","e", "F","f", "G","g", "H","h", "I","i", "J","j", "K","k", "L","l", "M","m", "N","n" ,"O","o", "P", "p", "Q","q", "R","r", "S","s", "T","t", "U","u", "V","v", "W","w", "X","x", "Y","y", "Z","z","A","a", "B","b", "C","c"," ","3","4","5","6","7","8","9","0","1","2"]
 
-    b = encryption_alph[c]
-    data.append(b)
-print("".join(data))
+
+if choice == 1:
+    name = input("Enter your message: ")
+    data = []
+    for i in name:
+        c = initial_alph.index(i)
+        b = encryption_alph[c]
+        data.append(b)
+    print("".join(data))
+elif choice == 2:
+    name = input("Enter your message: ")
+    data = []
+    for i in name:
+        c = encryption_alph.index(i)
+        b = initial_alph[c]
+        data.append(b)
+    print("".join(data))
+
+else:
+    sys.exit("Invalid input")
